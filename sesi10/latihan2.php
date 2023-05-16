7c41cafd3b25199ccf8417d7d197f5de59f12968
KadekArlandi <127810806+KadekArlandi@users.noreply.github.com>
2023-05-16 12:09:28 +0800

    Add files via upload

--- sesi10/latihan2.php
+++ sesi10/latihan2.php
@@ -7,7 +7,7 @@
     <title>LatihanForm2</title>
 </head>
 <body>
-<form name="Latihan1" method="post" onsubmit="return checkform(this)" action="">
+<form name="Latihan1" id="latihan1" method="post" onsubmit="return checkform(this)" action="">
     <div>
         NIM<br>
         <input type="text" name="txNIM" id="txNIM"><br>
@@ -27,11 +27,11 @@
     </div>
     <div>
         Hobi
-        <input type="checkbox" name="txhobibola" id="txhobibola"value="1">Sepak Bola
-        <input type="checkbox" name="txhobibulutangkis" id="txhobibulutangkis" value="2">Bulu Tangkis
-        <input type="checkbox" name="txhobivolly" id="txhobivolly" value="3">Bola Volly
-        <input type="checkbox" name="txhobibasket" id="txhobibasket" value="4">bolabasket
-        <input type="checkbox" name="txhobirenang" id="txhobirenang" value="5">Renang 
+        <input type="checkbox" name="txhobibola" id="txhobi"value="1">Sepak Bola
+        <input type="checkbox" name="txhobibulutangkis" id="txhobi" value="2">Bulu Tangkis
+        <input type="checkbox" name="txhobivolly" id="txhobi" value="3">Bola Volly
+        <input type="checkbox" name="txhobibasket" id="txhobi" value="4">bolabasket
+        <input type="checkbox" name="txhobirenang" id="txhobi" value="5">Renang 
     </div>
     <div>
         <button type="submit">KIRIM</button>
@@ -39,16 +39,26 @@
 </form>
 
 <script type="text/javascript">
-    function checkform(){
-        let frm = document.getElementById("Latihan2").elements
+   function checkform() {
+    let frm = document.getElementById('latihan1').elements;
 
-        let NIM = frm.txhobi
-        let JK  = frm.txJK
-        let Jurusan = frm.txJurusan
-        let Hobi = [
-            a
-        ]
-    }
+    let NIM =  frm.txNIM.value;
+    let JenisKelamin = frm.txJK.value;
+    let Jurusan = frm.txJurusan.value;
+    let hobi = [
+        frm.txhobi[0].checked,
+        frm.txhobi[1].checked,
+        frm.txhobi[2].checked,
+        frm.txhobi[3].checked,
+        frm.txhobi[4].checked
+    ]
+    console.log("NIM: " + NIM);
+    console.log("Jenis Kelamin: " + JenisKelamin);
+    console.log("Jurusan: " + Jurusan);
+    console.log("hobi: " + hobi);
+
+    return false;
+   }
 
 </script>
 </body>
